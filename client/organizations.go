2d1cc651d9bcee409b31ac4165c905c0d2678ca5
nolte <nolte07@googlemail.com>
2020-09-04 21:58:57 +0200

    add organisations methodes

--- client/organizations.go
+++ client/organizations.go
@@ -1,5 +1,11 @@
 package awx
 
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+)
+
 // OrganizationsService implements awx projects apis.
 type OrganizationsService struct {
 	client *Client
@@ -16,7 +22,7 @@ const organizationsAPIEndpoint = "/api/v2/organizations/"
 // ListOrganizations shows list of awx projects.
 func (p *OrganizationsService) ListOrganizations(params map[string]string) ([]*Organizations, *ListOrganizationsResponse, error) {
 	result := new(ListOrganizationsResponse)
-	resp, err := p.client.Requester.GetJSON(projectsAPIEndpoint, result, params)
+	resp, err := p.client.Requester.GetJSON(organizationsAPIEndpoint, result, params)
 	if err != nil {
 		return nil, result, err
 	}
@@ -26,4 +32,84 @@ func (p *OrganizationsService) ListOrganizations(params map[string]string) ([]*O
 	}
 
 	return result.Results, result, nil
+}
+
+// GetOrganizationsByID shows the details of a Organization.
+func (p *OrganizationsService) GetOrganizationsByID(id int, params map[string]string) (*Organizations, error) {
+	result := new(Organizations)
+	endpoint := fmt.Sprintf("%s%d/", organizationsAPIEndpoint, id)
+	resp, err := p.client.Requester.GetJSON(endpoint, result, params)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := CheckResponse(resp); err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
+
+// CreateOrganization creates an awx Organization.
+func (p *OrganizationsService) CreateOrganization(data map[string]interface{}, params map[string]string) (*Organizations, error) {
+	mandatoryFields = []string{"name"}
+	validate, status := ValidateParams(data, mandatoryFields)
+
+	if !status {
+		err := fmt.Errorf("Mandatory input arguments are absent: %s", validate)
+		return nil, err
+	}
+
+	result := new(Organizations)
+	payload, err := json.Marshal(data)
+	if err != nil {
+		return nil, err
+	}
+	resp, err := p.client.Requester.PostJSON(organizationsAPIEndpoint, bytes.NewReader(payload), result, params)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := CheckResponse(resp); err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
+
+// UpdateOrganization update an awx Organization.
+func (p *OrganizationsService) UpdateOrganization(id int, data map[string]interface{}, params map[string]string) (*Organizations, error) {
+	result := new(Organizations)
+	endpoint := fmt.Sprintf("%s%d", organizationsAPIEndpoint, id)
+	payload, err := json.Marshal(data)
+	if err != nil {
+		return nil, err
+	}
+	resp, err := p.client.Requester.PatchJSON(endpoint, bytes.NewReader(payload), result, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := CheckResponse(resp); err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
+
+// DeleteOrganization delete an awx Organization.
+func (p *OrganizationsService) DeleteOrganization(id int) (*Organizations, error) {
+	result := new(Organizations)
+	endpoint := fmt.Sprintf("%s%d", organizationsAPIEndpoint, id)
+
+	resp, err := p.client.Requester.Delete(endpoint, result, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := CheckResponse(resp); err != nil {
+		return nil, err
+	}
+
+	return result, nil
 }
